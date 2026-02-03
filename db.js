a88b69e7c5d40a036291ef60c715209cf7026e69
SunnyChauhan00 <sunnychauhan246745@gmail.com>
2026-02-03 15:14:57 +0530

    some update2

--- db.js
+++ db.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST || '',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'elibrary',
+    database: process.env.DB_NAME || '',
     multipleStatements: true // Allow multiple statements
 });
 
